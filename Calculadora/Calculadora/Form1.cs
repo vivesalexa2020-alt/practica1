6dc1b7329a4f5dfe42c291e522d052be96825eb6
alexandra <vivesalexa2020@gmail.com>
2026-05-04 22:14:56 -0600

    c 6

--- Calculadora/Calculadora/Form1.cs
+++ Calculadora/Calculadora/Form1.cs
@@ -1,46 +1,45 @@
-public partial class Form1 : Form, ICalculadoraVista
+using Calculadora.Vista;
+using System;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Calculadora
 {
-    public Form1()
+    public partial class Form1 : Form, ICalculadoraVista
     {
-        InitializeComponent();
-
-        buttonCalcular.Click += (sender, e) =>
+        public Form1()
         {
-            CalcularClicked?.Invoke(sender, e);
-        };
+            InitializeComponent();
+
+            buttonCalcular.Click += (sender, e) =>
+            {
+                CalcularClicked?.Invoke(sender, e);
+            };
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        public string Numero1
+        {
+            get => textBox1.Text;
+            set => textBox1.Text = value;
+        }
+
+        public string Numero2
+        {
+            get => textBox2.Text;
+            set => textBox2.Text = value;
+        }
+
+        public string Resultado
+        {
+            get => labelResultado.Text;
+            set => labelResultado.Text = value;
+        }
+
+        public event EventHandler CalcularClicked;
     }
-
-    private void Form1_Load(object sender, EventArgs e)
-    {
-        // Evento de carga vacío
-    }
-
-    public string Numero1
-    {
-        get => textBox1.Text;
-        set => textBox1.Text = value;
-    }
-
-    public string Numero2
-    {
-        get => textBox2.Text;
-        set => textBox2.Text = value;
-    }
-
-    public string Resultado
-    {
-        get => labelResultado.Text;
-        set => labelResultado.Text = value;
-    }
-
-    public event EventHandler CalcularClicked;
-
 }
-
-
-
-
-
-
-
-
+}
